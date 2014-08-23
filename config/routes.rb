da5a44fe4e92e5a02fdc9c2c6bcf2da7bc31cef1
Ahmed AbouElhamayed <alwahsh.ahmed@gmail.com>
2014-08-24 02:25:38 +0300

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   
   resources :groups do 
-    resources :deadlines, :users
+    resources :users
   end
 
   resources :users do
-    resources :groups, :deadlines
+    resources :groups
   end
 
   resources :deadlines
